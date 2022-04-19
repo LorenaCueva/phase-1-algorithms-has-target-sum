a383f5b3843a60b1c0c7a79cfe7617ef4b65a078
Lorena Cueva <lorenacueva@gmail.com>
2022-04-19 12:06:45 -0700

    commit tree 0eb34745a57857984e840b1c065ee83ff2fd11ec

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let i =0; i < array.length - 1; i++){
+    if(array.slice(i+1).includes(target - array[i])) return true
+  }
+  return false
 }
+
+function hasTargetSum2(array, target){
+  if(array.length === 2) return target - array[0] === array[1]
+  else{
+    array.slice(1).includes(target-array[0]) ?  true : hasTargetSum2(array.slice(1), target)
+  }
+  return true
+}
+  
 
 /* 
   Write the Big O time complexity of your function here
@@ -29,6 +41,26 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("RECURSIVE:");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum2([3, 8, 12, 4, 11, 7], 10));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum2([22, 19, 4, 6, 30], 25));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum2([4, 2], 3));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum2([4, 2], 6));
 }
 
 module.exports = hasTargetSum;
